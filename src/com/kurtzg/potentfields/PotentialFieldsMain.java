848a28279dae62c0613f286bafbda41a1f688dfe
U-MAGNETON\grnt426 <grnt426@Magneton.(none)>
2011-01-25 11:49:12 -0500

    First commit

---
+++ src/com/kurtzg/potentfields/PotentialFieldsMain.java
@@ -0,0 +1,23 @@
+package com.kurtzg.potentfields;
+
+/*
+ *
+ * Author:      Grant Kurtz
+ *
+ * Description: Initializes the Potential Field Map and supporting classes
+ *              through command line arguments.
+ */
+public class PotentialFieldsMain {
+
+    public static void main(String[] args){
+
+
+    }
+
+    /*
+     * Default Constructor
+     */
+    public PotentialFieldsMain(){
+
+    }
+}
